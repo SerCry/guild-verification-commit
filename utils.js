830490a857a7c557af77f9f0c7048855419c9406
SerCry <92796990+SerCry@users.noreply.github.com>
2026-03-19 17:29:17 +0700

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T10:28:59.982Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-19T10:29:16.461Z
+Commit: Patch API endpoint error handling
