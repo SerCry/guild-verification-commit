1f51cafaeaefda55d5b02f8c2de46b9b88aba252
SerCry <92796990+SerCry@users.noreply.github.com>
2026-03-19 17:28:59 +0700

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T10:28:56.130Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-19T10:28:58.240Z
+Commit: Fix CSS styling bug on mobile devices
